0e89f8eb45e71c688227865606c868a5fbef7952
TrieuPorches <80805409+NguyenTrieu903@users.noreply.github.com>
2025-07-21 21:36:37 +0700

    Delete dist directory

--- dist/utils/repository-analyzer.d.ts
+++
@@ -1,3 +0,0 @@
-import { RepositoryAnalysis } from "../types";
-export declare function analyzeRepository(repoPath: string): Promise<RepositoryAnalysis>;
-//# sourceMappingURL=repository-analyzer.d.ts.map
